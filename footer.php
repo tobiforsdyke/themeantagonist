31729756f7a043f79c353a16dde0ad804593bfa0
tobiforsdyke <tobiforsdyke@gmail.com>
2019-01-15 12:20:00 +0000

    Added menu functions and primary and secondary menus (part 3)

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
   <footer>
     <p>This is the footer</p>
+    <?php wp_nav_menu( array('theme_location'=>'secondary') ); ?>
   </footer>
 
   <?php wp_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,10 +1,16 @@
 <?php
 
 function antagonist_script_enqueue() {
-
   wp_enqueue_style( 'customstyle', get_template_directory_uri() . '/css/antagonist.css', array(), '1.0.0', 'all' );
   wp_enqueue_script( 'customjs', get_template_directory_uri() . '/js/antagonist.js', array(), '1.0.0', true );
-
 }
 
 add_action( 'wp_enqueue_scripts', 'antagonist_script_enqueue');
+
+function antagonist_theme_setup() {
+  add_theme_support( 'menus' );
+  register_nav_menu( 'primary', 'Primary Header Navigation' );
+  register_nav_menu( 'secondary', 'Footer Navigation' );
+}
+
+add_action( 'init', 'antagonist_theme_setup' );
