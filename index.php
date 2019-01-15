1bae2b458ca5cb71cf48f7a2d81571835c78b324
tobiforsdyke <tobiforsdyke@gmail.com>
2019-01-15 22:48:40 +0000

    Changed the index file to display date in UK format

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
       while( have_posts() ): the_post(); ?>
 
         <h3><?php the_title(); ?></h3>
-        <small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
+        <small>Posted on: <?php the_time('j F Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
         <p><?php the_content(); ?></p>
         <hr>
 
