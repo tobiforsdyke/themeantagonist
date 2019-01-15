18cfbf3afb1dbbf3ee840db1d10bcbff09702ea1
tobiforsdyke <tobiforsdyke@gmail.com>
2019-01-15 15:57:44 +0000

    Added post loop to index.php and custom classes to header.php

--- header.php
+++ header.php
@@ -5,6 +5,19 @@
     <title>Antagonist Theme</title>
     <?php wp_head(); ?>
   </head>
-  <body>
+
+  <!-- Adds specific class if on front page only otherwise different class -->
+  <!-- Remember Wordpress classes home as the one with blogroll on it -->
+  <!-- Use if( is_home() ) instead if wanting to change the blog -->
+  <?php
+    if( is_front_page() ):
+      $antagonist_classes = array('antagonist-class', 'my-class');
+    else:
+      $antagonist_classes = array('no-antagonist-class');
+    endif;
+  ?>
+
+  <!-- Adds dynamic classes to the body tag -->
+  <body <?php body_class( $antagonist_classes ); ?>>
 
     <?php wp_nav_menu( array('theme_location'=>'primary') ); ?>
