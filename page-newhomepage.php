f8629c3e06e12168e8ff498a72f9d312e1411486
tobiforsdyke <tobiforsdyke@gmail.com>
2019-01-30 13:21:04 +0000

    Started using query posts and create 2 new homepage templates, 1 that displays only the 3 most recent posts from 3 different categories

---
+++ page-newhomepage.php
@@ -0,0 +1,97 @@
+<?php
+/*
+  Template Name: New Homepage with queries
+*/
+get_header();?>
+
+<!-- This template displays the last three most recent posts, -->
+<!-- the first as a full width post and the next two underneath alongside the sidebar -->
+
+<div class="row">
+
+  <div class="col-xs-12">
+    <?php
+    // PRINT ONLY THE MOST RECENT ONE POST FEATURED FULL WIDTH
+      $args = array(
+        'type' => 'post',
+        'posts_per_page' => 1
+      );
+      $lastBlog = new WP_Query($args);
+
+      if( $lastBlog->have_posts() ):
+          while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+
+            <?php get_template_part('content',get_post_format() ); ?>
+
+      <?php endwhile; ?>
+      <?php endif;
+
+      wp_reset_postdata();
+
+    ?>
+  </div>
+
+  <div class="col-xs-12 col-sm-8">
+    <?php if( have_posts() ):
+        while( have_posts() ): the_post(); ?>
+
+          <!-- Calls the standard post format from content.php -->
+          <!-- Otherwise calls the get_post_format page (aside, image etc) -->
+          <?php get_template_part('content',get_post_format() ); ?>
+
+    <?php endwhile; ?>
+    <?php endif;
+
+    // PRINT NEXT 2 POSTS BUT NOT THE FIRST ONE USING OFFSET VARIBLE
+    $args = array(
+      'type' => 'post',
+      'posts_per_page' => 2,
+      'offset' => 1
+    );
+
+    $lastBlog = new WP_Query($args);
+
+    if( $lastBlog->have_posts() ):
+        while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+
+          <?php get_template_part('content',get_post_format() ); ?>
+
+    <?php endwhile; ?>
+    <?php endif;
+
+    wp_reset_postdata();
+
+    ?>
+
+    <hr />
+
+    <?php
+    // PRINT ONLY ALL THE NEWS TYPE 1's, POSTS PER PAGE -1 MEANS INFINITE
+    $args = array(
+      'type' => 'post',
+      'posts_per_page' => -1,
+      'category_name' => 'news-type-1'
+    );
+    $lastBlog = new WP_Query($args);
+
+    if( $lastBlog->have_posts() ):
+        while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+
+          <?php get_template_part('content',get_post_format() ); ?>
+
+    <?php endwhile; ?>
+    <?php endif;
+
+    wp_reset_postdata();
+
+    ?>
+
+  </div>
+
+  <div class="col-xs-12 col-sm-4">
+    <?php get_sidebar();?>
+  </div>
+
+</div>
+
+<?php get_footer();?>
---
+++ page-newhomepage2.php
@@ -0,0 +1,71 @@
+<?php
+/*
+  Template Name: New Homepage with last 3 featured news
+*/
+get_header();?>
+
+<div class="row">
+
+  <div class="col-xs-12 col-sm-4">
+    <?php
+    $args = array(
+      'type' => 'post',
+      'posts_per_page' => 1,
+      'category_name' => 'news-type-1'
+    );
+    $lastBlog = new WP_Query($args);
+
+    if( $lastBlog->have_posts() ):
+        while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+
+          <?php get_template_part('content',get_post_format() ); ?>
+
+    <?php endwhile; ?>
+    <?php endif;
+    wp_reset_postdata();
+    ?>
+  </div>
+
+  <div class="col-xs-12 col-sm-4">
+    <?php
+    $args = array(
+      'type' => 'post',
+      'posts_per_page' => 1,
+      'category_name' => 'news-type-2'
+    );
+    $lastBlog = new WP_Query($args);
+
+    if( $lastBlog->have_posts() ):
+        while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+
+          <?php get_template_part('content',get_post_format() ); ?>
+
+    <?php endwhile; ?>
+    <?php endif;
+    wp_reset_postdata();
+    ?>
+  </div>
+
+  <div class="col-xs-12 col-sm-4">
+    <?php
+    $args = array(
+      'type' => 'post',
+      'posts_per_page' => 1,
+      'category_name' => 'news-type-3'
+    );
+    $lastBlog = new WP_Query($args);
+
+    if( $lastBlog->have_posts() ):
+        while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+
+          <?php get_template_part('content',get_post_format() ); ?>
+
+    <?php endwhile; ?>
+    <?php endif;
+    wp_reset_postdata();
+    ?>
+  </div>
+
+</div>
+
+<?php get_footer();?>
