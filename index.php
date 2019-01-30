052399ed85e08a02e2a2dc47ded60e353cd7adbf
tobiforsdyke <tobiforsdyke@gmail.com>
2019-01-30 20:18:54 +0000

    Continued styling the blog page and removed padding from between the images

--- index.php
+++ index.php
@@ -10,18 +10,23 @@
         while( have_posts() ): the_post(); ?>
 
           <?php
-            if($i==0): $column = 12;
-            elseif($i > 0 && $i <= 2): $column = 6;
-            elseif($i > 2): $column = 4;
+            if($i==0): $column = 12; $class = ' ';
+            elseif($i > 0 && $i <= 2): $column = 6; $class = ' two-column-blog-padding';
+            elseif($i > 2): $column = 4; $class = ' three-column-blog-padding';
             endif;
           ?>
-            <div class="col-<?php echo $column; ?>">
-              <?php if( has_post_thumbnail() ): ?>
-                  <div class="thumbnail"><?php the_post_thumbnail('thumbnail'); ?></div>
-              <?php endif; ?>
+
+          <div class="col-<?php echo $column; echo $class; ?>">
+            <?php if( has_post_thumbnail() ):
+              $urlImg = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
+            endif; ?>
+            <div class="blog-element" style="background-image: url(<?php echo $urlImg; ?>);">
+
+
               <?php the_title( sprintf('<h1 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ),'</a></h1>' ); ?>
               <small><?php the_category(' '); ?></small>
             </div>
+          </div>
 
     <?php $i++; endwhile;
     endif; ?>
