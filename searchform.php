205f76d6ab70379778c96675cba3d972ac5a4c1c
tobiforsdyke <tobiforsdyke@gmail.com>
2019-02-01 16:00:06 +0000

    Tinkering with stylesheet and nav menu and search for styling

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <!-- This generates and styles the search form -->
 
 <form role="search" method="get" action="<?php echo home_url('/'); ?>">
-  <input type="search" class="form-control" placeholder="Search" value="<?php echo get_search_query() ?>" name="s" title="Search" />
+  <input type="search" class="form-control" placeholder="Search..." value="<?php echo get_search_query() ?>" name="s" title="Search" />
 </form>
