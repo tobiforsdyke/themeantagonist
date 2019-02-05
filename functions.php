85c0084b49a6df2218ee4957fbd637000864921d
tobiforsdyke <tobiforsdyke@gmail.com>
2019-02-05 10:23:30 +0000

    Changed Bootstrap files from minified versions to normal versions for testing

--- functions.php
+++ functions.php
@@ -6,11 +6,11 @@
 
 function antagonist_script_enqueue() {
   // CSS
-  wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.2.1', 'all' );
+  wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), '4.2.1', 'all' );
   wp_enqueue_style( 'customstyle', get_template_directory_uri() . '/css/antagonist.css', array(), '1.2.4', 'all' );
   // JS
   wp_enqueue_script( 'jquery' );
-  wp_enqueue_script( 'bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '4.2.1', true );
+  wp_enqueue_script( 'bootstrapjs', get_template_directory_uri() . '/js/bootstrap.js', array(), '4.2.1', true );
   wp_enqueue_script( 'customjs', get_template_directory_uri() . '/js/antagonist.js', array(), '1.1.3', true );
   // FONTAWESOME?
   wp_enqueue_style( 'fontawesomestyle', get_template_directory_uri() . '/fontawesome/css/all.min.css' );
